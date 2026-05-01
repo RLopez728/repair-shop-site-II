41bda26de689f8cf9790238b02dee84a504dd541
RixZC <92389036+RixZC@users.noreply.github.com>
2026-05-01 11:21:22 -0700

    Added links and polished sidebar

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ console.log("script loaded");
 sidebarBtn.addEventListener("click", () => {
     sidebar.classList.toggle("open");
 });
+
+/*---------------------------------------------------------------*/
+/*=============================Scroll============================*/
+/*---------------------------------------------------------------*/
+
+document.getElementById("next").addEventListener("click", () => {
+    const el = document.getElementById("scroll-target");
+    const headerHeight = 50;
+
+    const rect = el.getBoundingClientRect();
+    const absoluteTop = rect.top + window.pageYOffset;
+
+    const centeredPosition =
+        absoluteTop - (window.innerHeight / 2) + (rect.height / 2) - (headerHeight / 2);
+
+    window.scrollTo({
+        top: centeredPosition,
+        behavior: "smooth"
+    });
+});
